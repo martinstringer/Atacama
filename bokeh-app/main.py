b6a4683c1232bc0575baba3d930fd2fcc0d84308
martinstringer <116901130+martinstringer@users.noreply.github.com>
2022-11-06 21:27:59 +0000

    Add files via upload

--- bokeh-app/main.py
+++ bokeh-app/main.py
@@ -4,18 +4,19 @@
 import numpy as np
 import geopandas as gpd
 from pandas import read_csv
+from itertools import permutations
 from bokeh.plotting import figure, output_file, show
-from bokeh.models import Range1d,ColumnDataSource,TapTool,LabelSet
+from bokeh.models import Range1d,ColumnDataSource,TapTool,LabelSet,Button,NumberFormatter,Label
 from bokeh.tile_providers import get_provider, STAMEN_TERRAIN
 from bokeh.colors import RGB
 from bokeh.layouts import column,row,gridplot,widgetbox
 from bokeh.io import curdoc
 from bokeh.transform import linear_cmap
 from matplotlib import cm
-from bokeh.events import Tap,DoubleTap,MouseMove
+from bokeh.events import Tap,DoubleTap,MouseMove,ButtonClick
 from bokeh.models.widgets import DataTable,TableColumn
 
-data_folder = 'bokeh-app/data/' # This will be run from level above, so path has to include the folder that this code is located in.
+data_folder = 'bokeh-app/data/'
 
 # Read in the shapefile with the hexagon data
 hexagons = gpd.read_file(data_folder+'Topo_hex.shp')
@@ -73,7 +74,8 @@ p = figure(x_axis_type="mercator", y_axis_type="mercator",
            tools= "pan,wheel_zoom,tap",active_scroll="wheel_zoom",
            x_range=Range1d(xmin,xmax), y_range=Range1d(ymin,ymax),
            height=h,width=w,
-           title='Click to create demand point.          Double-click to create source')
+           title='Click to create demand point.      Double-click to create source')
+p.title.align = "center"          
 p.xgrid.visible = False
 p.ygrid.visible = False
 p.axis.visible = False
@@ -91,18 +93,26 @@ cost_palette = [RGB(*tuple(rgb)).to_hex() for rgb in cost_rgb]
 mapper = linear_cmap(field_name='z', palette=cost_palette,low=0,high=np.max(z))
 
 # Set up data source for source nodes
-water_source = ColumnDataSource(dict(xs=[],ys=[],xc=[],yc=[],ns=[]))
+water_source = ColumnDataSource(dict(xs=[],ys=[],xc=[],yc=[],ns=[],label=[]))
 
 # Set up data source for demand nodes 
 water_demand = ColumnDataSource(dict(xd=[],yd=[],nd=[],node=[]))
 
 # Set up data source for network 
-network = ColumnDataSource(dict(xn=[],yn=[],nn=[],cn=[],cmax=[],link=[],ref=[]))
+network = ColumnDataSource(dict(xn=[],yn=[],nn=[],cn=[],cmax=[],saving=[],link=[],ref=[]))
+
+# Set up data source for optimised
+optimised_network = ColumnDataSource(dict(xopt=[],yopt=[]))
 
 # Set up data source for new paths
 new_paths_data = ColumnDataSource(dict(xo=[],yo=[],co=[]))
 
+# Set up data source for the computer-optimised paths
 optimal_path_data = ColumnDataSource(dict(xp=[],yp=[],nopt=[],copt=[]))
+
+# Set up data source for total costs etc.
+totals_data = ColumnDataSource(dict(x=[0,0,0,0],y=[3,2,1,0],text=['','','',''],
+                                    color = ['cyan','blue','magenta','green']))
 
 # Add hexagon grid
 h = p.patches('xh', 'yh', alpha='al',selection_alpha='al',nonselection_alpha='al',source=grid_source,
@@ -121,18 +131,30 @@ optimal_path_line = p.multi_line('xp','yp',source=optimal_path_data,line_color='
 # Set up sources points
 sources = p.patches('xs','ys',hatch_color='cyan',hatch_pattern='horizontal_wave',
                     line_color='cyan',line_width=0.5,selection_line_alpha=al,nonselection_line_alpha=al,
-                    fill_color='blue',fill_alpha=al,nonselection_fill_alpha=al,selection_fill_alpha=al,
+                    fill_color='blue',fill_alpha=1,nonselection_fill_alpha=1,selection_fill_alpha=1,
                     alpha=al,nonselection_alpha=al,selection_alpha=al,source=water_source)
 # Set up demand points
 demand = p.circle('xd','yd',source=water_demand,fill_color='blue',alpha=1,size=20,line_color=None,
-                  fill_alpha=al,nonselection_fill_alpha=al,selection_fill_alpha=al)
+                  fill_alpha=1,nonselection_fill_alpha=1,selection_fill_alpha=1)
 # And numeric lables on demand points
 labels = LabelSet(x='xd',y='yd',text='node',text_color='white',text_align='center',text_baseline='middle',
                   x_offset=0,y_offset=0,source=water_demand,render_mode='canvas')
 p.add_layout(labels)
+# And numeric lables on demand points
+source_labels = LabelSet(x='xc',y='yc',text='label',text_color='white',text_align='center',text_baseline='middle',
+                  x_offset=0,y_offset=0,source=water_source,render_mode='canvas')
+p.add_layout(source_labels)
+
+# Set up "optimised" network lines
+optimised_lines = p.multi_line('xopt','yopt',source=optimised_network,line_color='green',line_width=4,alpha=al,
+                             selection_line_alpha=al,nonselection_line_alpha=al,line_alpha=al)
 
 # Set up list for all cells in network
 network_nodes = np.array([],dtype=int)
+
+
+total_cost = 0
+direct_cost = 0
 
 i_hover=Nh+2
 # Create rule for showing paths on hover
@@ -181,10 +203,11 @@ def show_paths(event):
                                                              co=[path_option_costs[k]]))
                 new_paths_data.data.update(new_path_options.data)     
             i_hover=i
+
         
 # Create rule for updating paths on click
 def create_demand_point(event):
-    global paths,path_costs,x,y,network_nodes
+    global paths,path_costs,x,y,network_nodes,total_cost,direct_cost
     i = grid_source.selected.indices[0]
     # Check that we don't already have this point in the network
     if (i in network_nodes):
@@ -211,26 +234,37 @@ def create_demand_point(event):
         if N_nodes>0:
             # Connect new node to existing network
             route = optimal_path_data.data['nopt']
+            cn = optimal_path_data.data['copt'][0]
+            cmax = np.min(new_paths_data.data['co'])
             new_segment = {
                 'nn' : [route],
                 'xn' : [x[route]],
                 'yn' : [y[route]],
-                'cn' : optimal_path_data.data['copt'],
-                'cmax' : [np.max(new_paths_data.data['co'])],
+                'cn' : [cn],
+                'cmax' : [cmax],
+                'saving': [cmax-cn],
                 'link' : [len(network.data['link'])+1],
                 'ref' : [node[-1]]
             }
+            total_cost += cn
+            direct_cost +=cmax
+            saved_cost = direct_cost-total_cost
             network.stream(new_segment)
             print('Added point:',network.data['link'],network.data['cn'])
             # Add cells on route to network nodes
             network_nodes = np.unique(np.append(network_nodes,route))
+            # Update totals shown in summary text
+            totals_data.data['text']=['Total cost (connecting directly to a source) = {:.2f}'.format(direct_cost),
+                  'Total cost (if sharing nework) = {:.2f}'.format(total_cost),
+                  'Total saved by sharing network = {:.2f}   ({:.0%})'.format(saved_cost,saved_cost/direct_cost),
+                  '']
         else:
             # For the first demand point, just update list of network nodes
             network_nodes = np.append(network_nodes,i)
-        print('Updated network nodes:',network_nodes)
+        print('Updated network nodes:',network_nodes,'Total cost so far=',total_cost)
             
 def create_source(event):
-    global x,y,network_nodes,xh,yh
+    global x,y,network_nodes,xh,yh,total_cost,direct_cost
     #print('Double Tap registered',event.x,event.y)
     d2 = (x-event.x)**2 + (y-event.y)**2
     i = np.argmin(d2)
@@ -244,23 +278,33 @@ def create_source(event):
         print('New water source added at cell:',i)
         #print('Existing network nodes:',network_nodes)
         # Check to see if there is any network yet
+        source_label = 'S'+str(len(water_source.data['xs'])+1)
         if network_nodes.size>0:
             # If there is, connect new source to network
             route = optimal_path_data.data['nopt']
+            cn = optimal_path_data.data['copt'][0]
+            cmax = np.min(new_paths_data.data['co'])
             new_segment = {
                 'nn' : [route],
                 'xn' : [x[route]],
                 'yn' : [y[route]],
-                'cn' : optimal_path_data.data['copt'],
-                'cmax' : [np.min(new_paths_data.data['co'])],
+                'cn' : [cn],
+                'cmax' : [cmax],
+                'saving' : [cmax-cn],
                 'link' : [len(network.data['link'])+1],
-                'ref' : ['S'+str(len(water_source.data['xs'])+1)]
+                'ref' : [source_label]
             }
             network.stream(new_segment)
+            total_cost += cn
+            direct_cost +=cmax
+            saved_cost = direct_cost-total_cost
             # Add cells on route to network nodes
             network_nodes = np.unique(np.append(network_nodes,route))
-            #if len(water_source.data['ns'])==0:
-                # If this is the first source, need to update network link costs.   
+            # Update totals shown in summary text
+            totals_data.data['text']=['Total cost (connecting directly to a source) = {:.2f}'.format(direct_cost),
+                  'Total cost (if sharing nework) = {:.2f}'.format(total_cost),
+                  'Total saved by sharing network = {:.2f}   ({:.0%})'.format(saved_cost,saved_cost/direct_cost),
+                  '']
         # Add this cell to list of sources
         xs = water_source.data['xs']
         xs.append(xh[i])
@@ -272,11 +316,13 @@ def create_source(event):
         yc.append(y[i])
         ns = water_source.data['ns']
         ns.append(i)
-        new_source = ColumnDataSource(dict(xs=xs,ys=ys,xc=xc,yc=yc,ns=ns))
+        label = water_source.data['label']
+        label.append(source_label)
+        new_source = ColumnDataSource(dict(xs=xs,ys=ys,xc=xc,yc=yc,ns=ns,label=label))
         water_source.data.update(new_source.data)
         # Add this cell to list of network nodes
         network_nodes=np.append(network_nodes,i)
-        print('Updated network nodes:',network_nodes)         
+        print('Updated network nodes:',network_nodes,'Total cost so far=',total_cost)         
     
 taptool = p.select(type=TapTool,renderers=[h])
 p.on_event(Tap,create_demand_point)
@@ -285,22 +331,73 @@ p.on_event(DoubleTap,create_source)
 hovertool = p.select(type=MouseMove,renderers=[h])
 p.on_event(MouseMove,show_paths)
 
-# Add table
-columns = [
-        TableColumn(field="ref", title="Link"),
-        TableColumn(field="cn", title="Cost if network shared"),
-        TableColumn(field="cmax", title="Cost to connect directly to source")
-    ]
-q = DataTable(source=network, columns=columns, width=w, height=250,index_position=None)
+# Add button to show alternative network
+def show_optimum(event):
+    global paths,path_costs,x,y,total_cost,direct_cost
+    print('Finding lower cost solution...')
+    points = np.append(water_source.data['ns'],water_demand.data['nd'])
+    Npoints =  points.size
+    min_cost = total_cost
+    if Npoints<5:
+        points_sequence = np.array(list(set(permutations(np.arange(Npoints)))))
+    else:
+        points_sequence = np.empty((1000,Npoints),dtype=int)
+        for i in range(points_sequence.shape[0]):
+            print(points_sequence.shape)
+            points_sequence[i,:] = np.random.choice(Npoints,Npoints,replace=False)
+    for i in range(points_sequence.shape[0]):
+        network_cost = 0
+        xopt = []
+        yopt = []
+        first_point = True
+        random_order =  points_sequence[i,:]
+        print('Working through',Npoints,'points in order:',random_order)
+        for jp in random_order:
+             j = points[jp]
+             if first_point:
+                 pipeline = np.delete(points,jp)
+             if j not in pipeline:
+                 kp = np.argmin(path_costs[j,pipeline])
+                 k = pipeline[kp]
+                 route  = paths[j,k]
+                 xopt.append(x[route])
+                 yopt.append(y[route])
+                 network_cost += path_costs[j,k]
+                 if first_point:
+                     pipeline = np.array(route)
+                     first_point = False
+                 else:
+                     pipeline = np.append(pipeline,route)
+                 #print('Connecting',jp,'th point',j,'Segment cost=',path_costs[j,k],'Pipeline:',pipeline)
+        print('Iteration',i,'Total cost=',network_cost)
+        if network_cost < min_cost:
+            optimised_network.data.update(ColumnDataSource(dict(xopt=xopt,yopt=yopt)).data)
+            min_cost = network_cost
+    print('Lowest cost solution:',min_cost)
+    saved_cost = direct_cost - min_cost
+    totals_data.patch({ 'text' :
+                        [(3, 'Potential saved by planning in advance = {:.2f}   ({:.0%})'.format(saved_cost,saved_cost/direct_cost))]})
+    #print(xopt,yopt)  
 
+# Add text explaining the total cost of the scenarios
+s = figure(width=w, height=130,tools="",outline_line_alpha=0,
+                    x_range=Range1d(0,10), y_range=Range1d(0,4),margin=(0,0,20,30))
+s.xgrid.visible = False
+s.ygrid.visible = False
+s.axis.visible = False
 
-# Add graph to illustrate
-r = figure(width=w, height=250,tools="")#,y_range=Range1d(0,1))
+summary = LabelSet(x='x', y='y', text='text',
+                    source=totals_data, text_color='color',text_font_size='14px',text_align='left')
+
+s.add_layout(summary)
+
+# Add graph to illustrate costs
+r = figure(width=w, height=250,tools="",margin=(0,0,30,20))#,y_range=Range1d(0,1))
 r.xaxis.major_tick_line_color = None
 r.xaxis.minor_tick_line_color = None
 r.xaxis.major_label_text_color = None
 r.circle(2,1,alpha=0)
-r.vbar(x='link',top='cmax',width=0.4,source=network,color='cyan',alpha=0.5*al,line_color=None)
+r.vbar(x='link',top='cmax',width=0.5,source=network,color='cyan',alpha=0.5*al,line_color=None)
 r.vbar(x='link',top='cn',width=0.4,source=network,color='blue',alpha=al,line_color=None)
 #r.circle('link','cn',source=network,fill_color='blue',alpha=al,size=20,line_color=None)
 bar_labels = LabelSet(x='link',y='cn',text='ref',text_color='white',text_align='center',text_baseline='top',
@@ -309,7 +406,26 @@ r.add_layout(bar_labels)
 r.xaxis.axis_label = 'Network segment'
 r.yaxis.axis_label = 'Cost'# (relative)'
 
-page = gridplot([[p,column(widgetbox(q),r)],[]],toolbar_location="left")
+# Add table
+columns = [
+        TableColumn(field="ref", title="Link",width=50),
+        TableColumn(field="cmax", title="Cost directly to source",formatter=NumberFormatter(format='0.00'),width=140),
+        TableColumn(field="cn", title="Cost if network shared",formatter=NumberFormatter(format='0.00'),width=140),
+        TableColumn(field="saving",title="Cost saved by sharing network",formatter=NumberFormatter(format='0.00'),width=160)
+    ]
+q = DataTable(source=network, columns=columns, width=w, height=250,fit_columns=False,index_position=None,margin=(40,0,0,40))
+
+
+# Add a button to show a lower cost solution
+b = Button(label="Show lower cost solution", button_type="success",width=200,margin=(0,0,0,200))
+b.on_event(ButtonClick,show_optimum)
+
+
+page = gridplot([[p,column(s,r,b,widgetbox(q))],[]],toolbar_location="left")
+
+curdoc().title = 'Simplified network tool'
 
 curdoc().add_root(page)
-curdoc().title = "Simplified network tool"   
+
+
+   
